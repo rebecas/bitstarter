5ade037f08c1ca020f428bd714d47c5dd27e48b7
= <=>
2013-07-09 22:26:03 +0000

    linked web.js and index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  fs.readFileSync('./index.html','utf8');
+  response.send(fs.readFileSync('./index.html','utf8'));
   /*response.send('Hello World2!');*/
 });
 
